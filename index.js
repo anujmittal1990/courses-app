a574281f82f985528b4e7ea211de83228c893076
Anuj Mittal <anujmittal1990@gmail.com>
2024-09-08 17:19:52 +0530

    Added Sample Code

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hi");
+let courses = ["Hindi", "English", "Sanskrit"];
+let students = {
+  name: ["Anuj", "Rachit", "Cocco"],
+  desig: "Senior 3",
+};
+
+courses.map((i) => console.log(i));
+console.log(`{name : ${students.name[0]}, desig:${students.desig}}`);
